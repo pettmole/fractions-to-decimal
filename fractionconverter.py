96957a639e159dd3c268963d30390a1ae4f969e5
Pettmole <84863024+pettmole@users.noreply.github.com>
2021-05-28 14:12:07 -0400

    Add files via upload

---
+++ fractionconverter.py
@@ -0,0 +1,67 @@
+def math():
+    print("input numerator")
+    numerator = int(input())
+    print("input denominator")
+    denominator = int(input())
+    answer = numerator / denominator
+    print(answer)
+    print("type 'y' for another question")
+
+
+print("press 'y' to start")
+start = input()
+if start == "y":
+    math()
+
+another_question = input()
+
+if another_question == "y":
+    math()
+    another_question = input()
+
+if another_question == "y":
+    math()
+another_question = input()
+
+if another_question == "y":
+    math()
+another_question = input()
+
+if another_question == "y":
+    math()
+another_question = input()
+
+if another_question == "y":
+    math()
+another_question = input()
+
+if another_question == "y":
+    math()
+another_question = input()
+
+if another_question == "y":
+    math()
+another_question = input()
+
+if another_question == "y":
+    math()
+another_question = input()
+
+if another_question == "y":
+    math()
+another_question = input()
+
+if another_question == "y":
+    math()
+another_question = input()
+
+if another_question == "y":
+    math()
+another_question = input()
+
+if another_question == "y":
+    math()
+another_question = input()
+
+if another_question == "y":
+    math()
